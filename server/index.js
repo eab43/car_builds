7fdd6b6fdabb92662179bb5e5a273e34131023d5
eab43 <elvin.a.bernales@gmail.com>
2021-08-20 21:15:00 -0400

    Deleted api keys and created .env

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -13,11 +15,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://EaB:EaB1437@cluster0.mshmu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
 const PORT = process.env.port || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
