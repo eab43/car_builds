00b82311d6dfc7f48bed87291f38ae865d891ec9
eab43 <elvin.a.bernales@gmail.com>
2021-08-21 02:31:39 -0400

    Changed CardMedia's padding to see more of image

--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -3,10 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
     media: {
         height: '100%',
-        paddingTop: '56.25%',
+        width: '100%',
+        paddingTop: '75%',
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
         backgroundBlendMode: 'darken',
-
     },
     border: {
         border: 'solid',
@@ -16,8 +16,6 @@ export default makeStyles((theme) => ({
     },
     card: {
         borderRadius: '15px',
-        maxWidth: 345,
-
     },
     overlay: {
         position: 'absolute',
--- client/src/components/Posts/styles.js
+++ client/src/components/Posts/styles.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-    mainContainer: {
+    Container: {
         display: 'flex',
         alignItems: 'center',
     },
